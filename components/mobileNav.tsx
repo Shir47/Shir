9462ace632b04b44ab29468ac372903008b1c2a4
Shir <82440535+Shir47@users.noreply.github.com>
2024-10-12 17:36:18 +1030

    Update mobileNav.tsx

--- components/mobileNav.tsx
+++ components/mobileNav.tsx
@@ -30,20 +30,20 @@ const MobileNav = () => {
       <SheetTrigger className="flex items-center p-2">
         <Menu className="h-8 w-8 text-blue-600" />
       </SheetTrigger>
-      <SheetContent className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg">
-        <div className="mb-8 flex items-center justify-center">
+      <SheetContent className="w-full sm:w-auto flex flex-col items-center bg-white p-4 sm:p-6 md:p-8 rounded-lg shadow-lg">
+        <div className="mb-4 flex items-center justify-center">
           <Link href="/">
-            <Image src="/logo.png" height={60.53} width={47.35} alt="logo" className="rounded-lg shadow-md" />
+            <Image src="/hero/logo.svg" height={50} width={40} alt="logo" className="rounded-lg shadow-md" />
           </Link>
         </div>
-        <nav className="mt-4 flex flex-col items-center justify-center gap-6">
+        <nav className="mt-4 flex flex-col items-center justify-center gap-4">
           {menu.map((item, index) => (
             <Link
               href={item.href}
               key={index}
-              className="text-lg font-semibold text-gray-800 capitalize transition-colors duration-300 hover:text-blue-600"
+              className="text-base sm:text-lg font-semibold text-gray-800 capitalize transition-colors duration-300 hover:text-blue-600"
             >
-              <span className="relative pb-2 transition-all after:absolute after:left-0 after:bottom-0 after:h-1 after:w-full after:bg-blue-600 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100">
+              <span className="relative pb-1 transition-all after:absolute after:left-0 after:bottom-0 after:h-1 after:w-full after:bg-blue-600 after:scale-x-0 after:transition-transform after:duration-300 hover:after:scale-x-100">
                 {item.name}
               </span>
             </Link>
